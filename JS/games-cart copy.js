b0ae40646f847996267c8359ed8253feac1126ba
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2022-11-10 21:22:04 +0100

    Update games-cart copy.js

--- JS/games-cart copy.js
+++ JS/games-cart copy.js
@@ -1,157 +1,92 @@
-/* Game Info */
+/* Containers */
 
-let container = document.querySelector("#game-container");
+let gameContainer = document.querySelector('#game-container');
+let cartContainer = document.querySelector('#cart-tablebody');
 
+/* Arrays */
 
 let product = [{
-    title: "Assassin's Creed: Valhalla",
-    price: "599,-",
-    photo: "ACV.jpg",
-    type: "Type: Download for PC",
-    availabe: "Available: Unlimited",
-    reviews: "4/5",
-    rated: "Rated: PEGI18",
-    gameURL: "acv.html"
+        title: "Assassin's Creed: Valhalla",
+        price: "599,-",
+        photo: "ACV.jpg",
+        type: "Download for PC",
+        availabe: "Available: Unlimited",
+        reviews: "4/5",
+        rated: "PEGI18",
+        gameURL: "acv.html"
 },
 {
-    title: "Boyfriend Dungeon",
-    price: "TBA",
-    photo: "BD.jpg",
-    type: "Type: Download for PC",
-    availabe: "Available: Unlimited",
-    reviews: "TBA",
-    rated: "Rated: TBA",
-    gameURL: "bd.html"
+        title: "Boyfriend Dungeon",
+        price: "TBA",
+        photo: "BD.jpg",
+        type: "Download for PC",
+        availabe: "Available: Unlimited",
+        reviews: "TBA",
+        rated: "TBA",
+        gameURL: "bd.html"
 },
 {
-    title: "Dragon Age: Origins",
-    price: "199,-",
-    photo: "DAO.jpg",
-    type: "Type: DVD-rom for PC",
-    availabe: "Available: Less than 5",
-    reviews: "4/5",
-    rated: "Rated: PEGI18",
-    gameURL: "dao.html"
+        title: "Dragon Age: Origins",
+        price: "199,-",
+        photo: "DAO.jpg",
+        type: "DVD-rom for PC",
+        availabe: "Available: Less than 5",
+        reviews: "4/5",
+        rated: "PEGI18",
+        gameURL: "dao.html"
 },
 {
-    title: "Resident Evil Village",
-    price: "699,-",
-    photo: "REVIII.jpg",
-    type: "Type: Download for PC",
-    availabe: "Available: Unlimited",
-    reviews: "5/5",
-    rated: "Rated: PEGI18",
-    gameURL: "reviii.html"
+        title: "Resident Evil Village",
+        price: "699,-",
+        photo: "REVIII.jpg",
+        type: "Download for PC",
+        availabe: "Available: Unlimited",
+        reviews: "5/5",
+        rated: "PEGI18",
+        gameURL: "reviii.html"
 }];
 
-/* Game List */
+console.log(product);
+
+/* Generate Product List */
 
 for (let i = 0; i < product.length; i++) {
-    container.innerHTML += '<div class="game-product">' +
-    '<a href="games/' + product[i].gameURL + 'class="game-title" title="' + product[i].title + '">' + product[i].title + '</a><br>' +
-    '<img src="products/' + product[i].photo + '" class=cover--small" alt="' + product[i].title + 'cover art">' +
-    '<p class="pricetag">$<span>' + product[i].price + '</span></p>' +
-    '<p>' + product[i].type + '</p>' +
-    '<p class="review">' + product[i].reviews + '</p>' +
-    '<p>' + product[i].rated + '</p>' +
-    '<p>' + product[i].availabe + '</p>' +
-    '<div class="cart-add">' +
-        '<input type="text" class="game-amount" name="amount" value="1"/>'+
-        '<input type="submit" value="Add to Cart" class="cta add-to-cart" onClick="addToCart(this)" />'+
-    '</div>'
-'</div>';
-}
+        gameContainer.innerHTML += '<div class="game-product">' +
+                '<a href="games/' + product[i].gameURL + '" class="game-title grid-2" alt="' + product[i].title + '">' + product[i].title + '</a><br>' +
+                '<p class="review grid-2">Rated ' + product[i].reviews + '</p>' +
+                '<a href="games/' + product[i].gameURL + '" class="game-title grid-2" alt="' + product[i].title + '"><img src="products/' + product[i].photo + '" class="cover--small" alt="' + product[i].title + ' cover art"></a><br>' +
+                '<p class="grid-2">' + product[i].type + '</p>' +
+                '<p class="pricetag grid-2">$<span>' + product[i].price + '</span></p>' +
+                '<p class="grid-2">' + product[i].rated + '</p>' +
+                '<div class="cart-add grid-2">' +
+                        '<label for="amount"></label>' +
+                        '<select name="amount" class="game-amount">'+
+                        '<option value="1">1</option>' +
+                        '<option value="2">2</option>' +
+                        '<option value="3">3</option>' +
+                        '<option value="4">4</option>' +
+                        '<option value="5">5</option>' +
+                        '<input type="submit" value="Add" class="cta add-to-cart" onClick="addToCart(this)" />'+
+                '</div>'
+        '</div>';
 
+        function addToCart() {
+                if(localStorage.getItem('product')) {
+                        product = JSON.parse(localStorage.getItem('product'));
+                }
+                product.push({'productId' : product.id + 1, image : product.gameURL});
+                localStorage.setItem('product', JSON.stringify(product));
+        }
 
-/* Cart Functions */
-
-function addToCart() {
-    var products = [];
-
-    if(localStorage.getItem('products')) {
-        products = JSON.parse(localStorage.getItem('products'));
-    }
-    products.push({'productID' : productID + 1, image : '<imageLink>'});
-    localStorage.setItem('products', JSON.stringify(products));
-
-
-
-
-
-
-
-
-
-
+        function removeFromCart() {
+                let storageProducts = JSON.parse(localStorage.getItem('product'));
+                let product = storageProducts.filter(product => product.productId !== productId);
+                localStorage.setItem('product', JSON.stringify(product));
+        }
+};
 
 
 
-/* Cart Functions */
 
-function addToCart(target) {
-    var gameProduct =(target).closest('div.game-product');
 
-    var price = (gameProduct).find('.price span').text();
-    var title = (gameProduct).find('.title').text();
-    var amount = (gameProduct).find('.game-amount').text();
-
-    var cartitem = {
-        title: title,
-        price: price,
-        amount: amount
-    };
-    var cartitemJSON = JSON.stringify(cartitem);
-
-    var cartArray = new Array();
-    if (sessionStorage.getitem('shopping-cart')) {
-        cartArray = JSON.parse(sessionStorage.getitem('shopping-cart'));
-    }
-    cartArray.push(cartitemJSON);
-
-    var cartJSON = JSON.stringify(cartArray);
-    sessionStorage.setitem('shopping-cart', cartJSON);
-    showCartTable();
-}
-
-function emptyCart() {
-    if (sessionStorage.getitem('shopping-cart')) {
-        sessionStorage.removeitem('shopping-cart');
-        showCartTable();
-    }
-}
-
-function showCartTable() {
-    var cartRowHTML = "";
-    var gameCount= 0;
-    var grandTotal = 0;
-
-    var price = 0;
-    var amount = 0;
-    var subTotal = 0;
-
-    if (sessionStorage.getitem('shopping-cart')) {
-        var shoppingCart = JSON.parse(sessionStorage.getitem('shopping-cart'));
-        gameCount = shoppingCart.length;
-
-        shoppingCart.forEach(function(game) {
-            var cartitem = JSON.parse(game);
-            price = parseFloat(cartitem.price);
-            amount = parseInt(cartitem.amount);
-            total = price * amount
-
-            carRowHTML += '<tr>' +
-            '<td><img src="products/' + product[i].photo + '" class=cover--smallest" alt="' + product[i].title + 'cover art"></td>' +
-            '<td>$' + product[i].title + '</td>' +
-            '<td>$' + price.toFixed(2) + '</td>' +
-            '<td>$' + amount + '</td>' +
-            '<td>$' + subTotal.toFixed(2) + '</td>' +
-            '</tr>';
-
-            grandTotal += subTotal;
-        });
-    }
-
-    $('$cartTableBody').html(cartRowHTML);
-    $('#gameCount').text(gameCount);
-    $('#totalAmount').text("$" + grandTotal.toFixed(2));
-}
+/* Generate Cart Items */
