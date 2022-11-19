d5c13005fca0a33ee40353abd197f520f272f9f1
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2022-11-19 14:21:01 +0100

    Changed product.js and product.html
    Made changes to the product.html and product.js files to hopefully be one step closer to getting a generated html result.

--- js/product.js
+++ js/product.js
@@ -8,6 +8,8 @@ let h1 = document.querySelector("h1");
 
 let title = document.querySelector("title");
 
+let crumbs = document.querySelector(".crumbs");
+
 productContainer.innerHTML = "";
 
 async function getProduct() {
@@ -18,66 +20,64 @@ async function getProduct() {
 
         const product = await productResponse.json();
 
-            for(let i = 0; i < product.length; i++) {
+            console.log(product);
 
-                console.log(product);
-
-                productContainer.innerHTML += `<table>
-                    <tr class="border-top">
-                        <td rowspan="6">
-                            <img src="${product[0].images[0].src}" alt="${product[i].name} Cover" class="cover--big">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p class="italic">${product[i].attributes[4].name} ${product[i].attributes[4].options[0]}</p>
-                        </td>
-                        <td>
-                            <p class="italic">${product[i].attributes[4].name} ${product[i].attributes[4].options[0]}</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p class="review">${product[i].average_rating}</p>
-                        </td>
-                        <td>
-                            <a href="*" class="italic">Read reviews (${product[i].rating_count}</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p class="pricetag">$ ${product[i].price}</p>
-                        </td>
-                        <td>
-                            <a href="*" title="Add product"><button class="cta">Add</button></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>${product[i].attributes[2].name} ${product[i].attributes[2].options[0]}</p>
-                        </td>
-                        <td>
-                            <p>${product[i].attributes[3].name} ${product[i].attributes[3].options[0]}</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>${product[i].attributes[0].name} ${product[i].attributes[0].options[0]}</p>
-                        </td>
-                        <td>
-                            <p>${product[i].attributes[1].name} ${product[i].attributes[1].options[0]}</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="3">
-                            ${product[i].description}
-                        </td>
-                    </tr>
-                </table>`
+            productContainer.innerHTML += `<table>
+                <tr class="border-top">
+                    <td rowspan="6">
+                        <img src="${product.images[0].src}" alt="${product.name} Cover" class="cover--big">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="italic">${product.attributes[4].name} ${product.attributes[4].options[0]}</p>
+                    </td>
+                    <td>
+                         <p class="italic">${product.attributes[4].name} ${product.attributes[4].options[0]}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="review">${product.average_rating}</p>
+                    </td>
+                    <td>
+                        <a href="*" class="italic">Read reviews (${product.rating_count}</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p class="pricetag">$ ${product.price}</p>
+                    </td>
+                    <td>
+                        <a href="*" title="Add product"><button class="cta">Add</button></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>${product.attributes[2].name} ${product.attributes[2].options[0]}</p>
+                    </td>
+                    <td>
+                        <p>${product.attributes[3].name} ${product.attributes[3].options[0]}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>${product.attributes[0].name} ${product.attributes[0].options[0]}</p>
+                    </td>
+                    <td>
+                        <p>${product.attributes[1].name} ${product.attributes[1].options[0]}</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                            ${product.description}
+                    </td>
+                </tr>
+            </table>`
                 
-                title.innerHTML += `Game Hub | ${product[i].name}`
-                h1.innerHTML += `${product[i].name}`
-                }
+            title.innerHTML += `Game Hub | ${product.name}`
+            h1.innerHTML += `${product.name}`
+            crumbs.innerHTML += `<a href="index.html" title="Home">Home page</a> - <a href="games.html" title="Games">Games list</a> - ${product.name}`
     }
     catch(error) {
                     console.log("An error has occurred");
