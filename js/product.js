63b88f750aa17265f6e061c6c658bb65b6d65066
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2022-11-19 10:46:34 +0100

    Tried different changes to JS
    Tried different small changes to product.js to see if it generates html code. Still not working.

--- js/product.js
+++ js/product.js
@@ -22,6 +22,8 @@ let h1 = document.querySelector("h1");
 
 let title = document.querySelector("title");
 
+productContainer.innerHTML = "";
+
 async function getProduct() {
 
     try {
@@ -30,35 +32,35 @@ async function getProduct() {
 
         const product = await productResponse.json();
 
-            for(let j = 0; j < product.length; j++) {
+            for(let i = 0; i < product.length; i++) {
 
                 console.log(product);
 
                 productContainer.innerHTML += `<table>
                     <tr class="border-top">
                         <td rowspan="6">
-                            <img src="${product[j].images[0].src}" alt="${product[j].name} Cover" class="cover--big">
+                            <img src="${product[0].images[0].src}" alt="${product[i].name} Cover" class="cover--big">
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <p class="italic">${product[j].attributes[4].name} ${product[j].attributes[4].options[0]}</p>
+                            <p class="italic">${product[i].attributes[4].name} ${product[i].attributes[4].options[0]}</p>
                         </td>
                         <td>
-                            <p class="italic">${product[j].attributes[4].name} ${product[j].attributes[4].options[0]}</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p class="review">${product[j].average_rating}</p>
-                        </td>
-                        <td>
-                            <a href="*" class="italic">Read reviews (${product[j].rating_count}</a>
+                            <p class="italic">${product[i].attributes[4].name} ${product[i].attributes[4].options[0]}</p>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <p class="pricetag">$ ${product[j].price}</p>
+                            <p class="review">${product[i].average_rating}</p>
+                        </td>
+                        <td>
+                            <a href="*" class="italic">Read reviews (${product[i].rating_count}</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="pricetag">$ ${product[i].price}</p>
                         </td>
                         <td>
                             <a href="*" title="Add product"><button class="cta">Add</button></a>
@@ -66,29 +68,29 @@ async function getProduct() {
                     </tr>
                     <tr>
                         <td>
-                            <p>${product[j].attributes[2].name} ${product[j].attributes[2].options[0]}</p>
+                            <p>${product[i].attributes[2].name} ${product[i].attributes[2].options[0]}</p>
                         </td>
                         <td>
-                            <p>${product[j].attributes[3].name} ${product[j].attributes[3].options[0]}</p>
+                            <p>${product[i].attributes[3].name} ${product[i].attributes[3].options[0]}</p>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <p>${product[j].attributes[0].name} ${product[j].attributes[0].options[0]}</p>
+                            <p>${product[i].attributes[0].name} ${product[i].attributes[0].options[0]}</p>
                         </td>
                         <td>
-                            <p>${product[j].attributes[1].name} ${product[j].attributes[1].options[0]}</p>
+                            <p>${product[i].attributes[1].name} ${product[i].attributes[1].options[0]}</p>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="3">
-                            ${product[j].description}
+                            ${product[i].description}
                         </td>
                     </tr>
                 </table>`
                 
-                title.innerHTML += `Game Hub | ${product[j].name}`
-                h1.innerHTML += `${product[j].name}`
+                title.innerHTML += `Game Hub | ${product[i].name}`
+                h1.innerHTML += `${product[i].name}`
                 }
     }
     catch(error) {
